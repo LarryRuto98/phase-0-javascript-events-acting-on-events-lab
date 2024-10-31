b0a85e2ce50277116dd07b48cec8eb33cac620e4
larryruto98 <larryruto@gmail.com>
2024-10-31 20:13:17 +0300

    events acting on events lab completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "")
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0){
+        dodger.style.left = `${left -1}px`;
+    }
+}
+document.addEventListener("keydown", function(e){
+    if(e.key === "ArrowLeft"){
+        moveDodgerLeft();
+    }
+});
+
+const dodgerR = document.getElementById('dodger');
+function moveDodgerRight(){
+    const leftNumbers = dodgerR.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left < 400){
+        dodgerR.style.left =`${left + 1}px`
+    }
+}
+document.addEventListener("keydown", function(e){
+    if(e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+});
